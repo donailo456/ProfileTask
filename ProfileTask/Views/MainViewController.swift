510eab361ada1aa130c46ecb2f92191af12fda2d
Danil <118375572+donailo456@users.noreply.github.com>
2024-03-28 15:56:46 +0300

    add Coordinator

--- ProfileTask/Views/MainViewController.swift
+++ ProfileTask/Views/MainViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    // MARK: - Internal properties
+    
+    var viewModel: MainViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
